21693d9fe1fed6161f07783730527754a74ca376
Juklok <107051800+JoshuaKlot@users.noreply.github.com>
2024-11-09 07:32:02 -0500

    Add files via upload

--- platformer/Scripts/bullet.gd
+++ platformer/Scripts/bullet.gd
@@ -3,6 +3,16 @@ extends CharacterBody2D
 var direction = Vector2.ZERO
 const bulletSpeed = 300.0
 
+func _ready() -> void:
+	var timer = get_node("Timer")
+	if timer:
+		timer.start()
+	else:
+		print("Timer not found")
+
 func _physics_process(delta: float) -> void:
-    velocity = direction * bulletSpeed
-    move_and_slide()
+	velocity = direction * bulletSpeed
+	move_and_slide()
+
+func _on_Timer_timeout() -> void:
+	queue_free()
--- platformer/Scripts/player.gd
+++ platformer/Scripts/player.gd
@@ -7,25 +7,24 @@ const bullet_speed = 300.0
 var lastdirection = Vector2.DOWN
 
 func _physics_process(delta: float) -> void:
-    var bullet = preload("res://scenes/bullet.tscn")
-    var main = get_node("bulletContainer")
+	var bullet = preload("res://scenes/bullet.tscn")
+	var main = get_node("bulletContainer")
 
-    if Input.is_action_pressed("shoot"):
-        var instance = bullet.instantiate()
-        main.add_child(instance)
-        instance.global_position = global_position
-        instance.direction = lastdirection
+	if Input.is_action_just_pressed("shoot"):
+		var instance = bullet.instantiate()
+		main.add_child(instance)
+		instance.global_position = global_position
+		instance.direction = lastdirection
 
-    var direction := Input.get_axis("ui_left", "ui_right")
-    var direction2 := Input.get_axis("ui_up", "ui_down")
+	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction2 := Input.get_axis("ui_up", "ui_down")
 
-    if direction or direction2:
-        velocity.x = direction * SPEED
-        velocity.y = direction2 * SPEED
-        lastdirection = Vector2(direction, direction2).normalized()
-    else:
-        velocity.x = move_toward(velocity.x, 0, SPEED)
-        velocity.y = move_toward(velocity.y, 0, SPEED)
+	if direction or direction2:
+		velocity.x = direction * SPEED
+		velocity.y = direction2 * SPEED
+		lastdirection = Vector2(direction, direction2).normalized()
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
-    move_and_slide()
-
+	move_and_slide()
