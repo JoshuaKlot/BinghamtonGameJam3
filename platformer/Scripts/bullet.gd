4ff269440ac1c13be401a443cc5ff4e718a42876
Juklok <107051800+JoshuaKlot@users.noreply.github.com>
2024-11-09 07:16:32 -0500

    Update bullet.gd

--- platformer/Scripts/bullet.gd
+++ platformer/Scripts/bullet.gd
@@ -1,16 +1,8 @@
 extends CharacterBody2D
 
-var direction=Vector2(0,0)
-const bulletSpeed=300.0
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var direction = Vector2.ZERO
+const bulletSpeed = 300.0
 
 func _physics_process(delta: float) -> void:
-	velocity.x=direction.x*bulletSpeed
-	velocity.y=direction.y*bulletSpeed
+    velocity = direction * bulletSpeed
+    move_and_slide()
