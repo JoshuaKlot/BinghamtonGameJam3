75b2380ef352e878dcbd9a243487263cbfee1af6
CTBlue <connorkister8@gmail.com>
2024-11-09 13:45:18 -0500

    scene transition
    it works YIPPPEEE

--- platformer/Scripts/move_scene.gd
+++ platformer/Scripts/move_scene.gd
@@ -4,5 +4,5 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("Entered Collision")
-	#$PopupMenu.popup()
+	get_tree().change_scene_to_file("res://scenes/move_menu.tscn")
 	#pass # Replace with function body.
