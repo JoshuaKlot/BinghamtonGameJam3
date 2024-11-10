03c15b60bcda66423832196f79a31c74f9ca009d
Juklok <107051800+JoshuaKlot@users.noreply.github.com>
2024-11-10 13:23:31 -0500

    Update player.gd

--- binghamtongamejam3-main/Scripts/player.gd
+++ binghamtongamejam3-main/Scripts/player.gd
@@ -10,8 +10,10 @@ const bullet_speed = 300.0
 var lastdirection = Vector2.DOWN
 var bullet = preload("res://scenes/bullet.tscn")
 @onready var main = get_node("bulletContainer")
-var shootTimer = 0.0
-var isShooting = true
+
+var timer: Timer
+var baseAttackSpeed = 1.2
+var attackSpeed = 1.2
 
 var health = 3
 var baseHealth = 3
@@ -19,9 +21,7 @@ var baseHealth = 3
 var baseCritChance = 0.10
 var critChance = 0.10
 
-var attackSpeed = 5
-
-var allPowersArray = [50,10,5,0,10,0,0,0,0,0,0,0] #all the powers
+var allPowersArray = [50,10,5,0,10,6,0,0,0,0,0,0] #all the powers
 #all indices and their powers
 #index 0 = 
 #index 1 = 
@@ -36,17 +36,20 @@ var allPowersArray = [50,10,5,0,10,0,0,0,0,0,0,0] #all the powers
 
 #Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	shooting(delta)
 	if allPowersArray[0]:  #damage up
 		damage = basedamage + allPowersArray[0]
+	#allPowersArray[1] is for pellet size down below
 	if allPowersArray[2]:  #critical hits
 		critChance = baseCritChance + (0.05 * allPowersArray[2])
 	if allPowersArray[3]:  #Max HP up
-		pass
+		health = baseHealth + (allPowersArray[3])
 	if allPowersArray[4]:  #runSpeed up
 		SPEED = baseSpeed + (10 * allPowersArray[4])
-	if allPowersArray[5]:  #
-		pass
-	if allPowersArray[6]:
+	if allPowersArray[5]:  #attackSpeed
+		attackSpeed = baseAttackSpeed - (0.1 * allPowersArray[5])
+		timer.wait_time = attackSpeed
+	if allPowersArray[6]: 
 		pass
 	if allPowersArray[7]:
 		pass
@@ -54,6 +57,8 @@ func _process(delta: float) -> void:
 		pass
 	if allPowersArray[9]:
 		pass
+	
+	
 func _physics_process(delta: float) -> void:
 	
 	look_at(get_global_mouse_position())
@@ -70,14 +75,15 @@ func calculateCrit():
 	else:
 		damage = 2 * basedamage
 		
-func take_damage():
+func damage_taken():
 	print(health)
 	health = health - 1
 	
 func shootBullet():
 	var instance = bullet.instantiate()
 	if allPowersArray[1]:  #BASED ON PELLET POWER size up
-		instance.scale = Vector2(5,5)
+		var bulletsize = 1 + (0.5 * allPowersArray[1])
+		instance.scale = Vector2(bulletsize,bulletsize)
 	calculateCrit()
 	get_tree().root.add_child(instance)
 	instance.global_position = global_position
@@ -100,14 +106,26 @@ func move():
 	
 func shooting(delta):
 	if Input.is_action_pressed("shoot"):
-		if isShooting:
-			shootTimer += delta
-			if shootTimer > attackSpeed:
-				shootBullet()
-				shootTimer = 0
-			else:
-				shootBullet()
-				isShooting = true
-				shootTimer = 0
-	if Input.is_action_just_released("shoot"):
-		isShooting = false
+		if timer.is_stopped():
+			shootBullet()
+			timer.start()
+	#if Input.is_action_pressed("shoot"):
+	#	if isShooting:
+	#		print("shootTimer1: "  + str(shootTimer))
+	#		shootTimer += delta
+	#		if shootTimer > 1:
+	#			print("shootTimer2: "  + str(shootTimer))
+	#			shootBullet()
+	#			shootTimer = 0
+	#		else:
+	#			print("shootTimer3: "  + str(shootTimer))
+	#			shootBullet()
+	#			isShooting = true
+	#			shootTimer = 0
+	#if Input.is_action_just_released("shoot"):
+	#	isShooting = false
+
+func _ready():
+	timer = Timer.new()
+	timer.one_shot = true
+	add_child(timer)
