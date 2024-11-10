916d4b30f5c2866f6c379d69df9344264aab92ff
Juklok <107051800+JoshuaKlot@users.noreply.github.com>
2024-11-10 13:21:26 -0500

    Update bullet.gd

--- binghamtongamejam3-main/Scripts/bullet.gd
+++ binghamtongamejam3-main/Scripts/bullet.gd
@@ -1,19 +1,21 @@
 extends CharacterBody2D
 
-var direction = Vector2.ZERO
-const bulletSpeed = 300.0
+const bulletSpeed = 200.0
 func _ready() -> void:
-	look_at(get_viewport().get_mouse_position())
+    look_at(get_viewport().get_mouse_position())
 
 func _physics_process(delta: float) -> void:
-	velocity = Vector2(1, 0).rotated(rotation) * bulletSpeed
-	move_and_slide()
+    velocity = Vector2(1, 0).rotated(rotation) * bulletSpeed
+    move_and_slide()
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	#when it hits another area2d (enemy's)
-	#enemy takes damage
-	if area.get_parent().has_method("take_damage"):
-		area.get_parent().take_damage()
-	
-	queue_free()
+    #when it hits another area2d (enemy's)
+    #enemy takes damage
+    if area.get_parent().has_method("take_damage"):
+        area.get_parent().take_damage()
+        queue_free()
+    if area.get_parent().has_method("damage_taken"): 
+        pass
+    else:
+        queue_free()
