363a22356832e1ed816605f155b9df202ba210e6
BenKlotzkin <bklotzk1@binghamton.edu>
2024-11-09 19:36:20 -0500

    THISSHOULDBEGOOD
    FIX HITTING ENEMIES DO IT NOWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW

---
+++ platformer/Scripts/enemy.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@onready var player = %Player
+var SPEED = 65
+
+var HEALTH = 5
+
+func _ready():
+	add_to_group("enemy")
+
+func _physics_process(delta: float) -> void:
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * SPEED
+	move_and_slide()
+	
+
+
+func _process(delta: float) -> void:
+	if HEALTH == 0:
+		queue_free()
+
+
+func take_damage():
+	print(player.damage)
+	HEALTH = HEALTH - player.damage
